4006666414d9028f54ec360e21afdfdb6524dc7f
hunCloud <ngochungnguyen256@gmail.com>
2020-10-10 17:14:15 +0700

    fix mysqli_query function in theloaitheochude.php

--- app/src/main/java/hungnn/example/cloud/Model/Album.java
+++ app/src/main/java/hungnn/example/cloud/Model/Album.java
@@ -5,51 +5,52 @@ import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 
+
 public class Album implements Serializable {
 
-@SerializedName("IdAlbum")
-@Expose
-private String idAlbum;
-@SerializedName("TenAlbum")
-@Expose
-private String tenAlbum;
-@SerializedName("TenCaSiAlbum")
-@Expose
-private String tenCaSiAlbum;
-@SerializedName("HinhAlbum")
-@Expose
-private String hinhAlbum;
+    @SerializedName("IdAlbum")
+    @Expose
+    private String idAlbum;
+    @SerializedName("TenAlbum")
+    @Expose
+    private String tenAlbum;
+    @SerializedName("TencasiAlbum")
+    @Expose
+    private String tencasiAlbum;
+    @SerializedName("HinhanhAlbum")
+    @Expose
+    private String hinhanhAlbum;
 
-public String getIdAlbum() {
-return idAlbum;
-}
+    public String getIdAlbum() {
+        return idAlbum;
+    }
 
-public void setIdAlbum(String idAlbum) {
-this.idAlbum = idAlbum;
-}
+    public void setIdAlbum(String idAlbum) {
+        this.idAlbum = idAlbum;
+    }
 
-public String getTenAlbum() {
-return tenAlbum;
-}
+    public String getTenAlbum() {
+        return tenAlbum;
+    }
 
-public void setTenAlbum(String tenAlbum) {
-this.tenAlbum = tenAlbum;
-}
+    public void setTenAlbum(String tenAlbum) {
+        this.tenAlbum = tenAlbum;
+    }
 
-public String getTenCaSiAlbum() {
-return tenCaSiAlbum;
-}
+    public String getTencasiAlbum() {
+        return tencasiAlbum;
+    }
 
-public void setTenCaSiAlbum(String tenCaSiAlbum) {
-this.tenCaSiAlbum = tenCaSiAlbum;
-}
+    public void setTencasiAlbum(String tencasiAlbum) {
+        this.tencasiAlbum = tencasiAlbum;
+    }
 
-public String getHinhAlbum() {
-return hinhAlbum;
-}
+    public String getHinhanhAlbum() {
+        return hinhanhAlbum;
+    }
 
-public void setHinhAlbum(String hinhAlbum) {
-this.hinhAlbum = hinhAlbum;
-}
+    public void setHinhanhAlbum(String hinhanhAlbum) {
+        this.hinhanhAlbum = hinhanhAlbum;
+    }
 
 }
